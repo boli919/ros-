d5b24c05009d3ed5c972ac1e778713fe3e3edda8
boli919 <1722491954@qq.com>
2024-07-22 14:52:31 +0800

    commitment

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/boruili/Desktop/ros1lr/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/boruili/Desktop/ros1lr/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/boruili/Desktop/ros1lr/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/boruili/Desktop/ros1lr/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/boruili/Desktop/ros1lr/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/boruili/Desktop/ros1lr/src:$ROS_PACKAGE_PATH"
